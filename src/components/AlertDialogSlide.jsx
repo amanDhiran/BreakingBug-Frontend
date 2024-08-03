4a3ab0bcb7ef8feb2f6f91d030b44af905d689c1
amanDhiran <amandhiran201@gmail.com>
2024-08-03 20:32:50 +0530

    fix taskHandler

--- src/components/AlertDialogSlide.jsx
+++ src/components/AlertDialogSlide.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Slide } from "@mui/material";
 import { GreenButton, RedButton } from "../utils/buttonStyles";
 
-const AlertDialogSlide = ({ dialog, showDialog, setShowDialog }) => {
+const AlertDialogSlide = ({ dialog, showDialog, setShowDialog, taskHandler }) => { // *** bug ***
     const handleClose = () => {
         setShowDialog(false);
     };
